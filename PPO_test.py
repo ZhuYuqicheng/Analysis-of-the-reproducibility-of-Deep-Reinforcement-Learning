14f657fe9ff7d20429040ad9a289260d50bb9394
Yuqicheng Zhu <yuqicheng.zhu@tum.de>
2021-06-05 00:15:13 +0200

    first commit

---
+++ PPO_test.py
@@ -0,0 +1,125 @@
+import os
+import imageio
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch as th
+from torch import nn
+import scipy.stats as stats
+
+import gym
+from stable_baselines3.ppo.ppo import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common import results_plotter
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
+from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.vec_env import VecVideoRecorder, DummyVecEnv
+from stable_baselines3.common.policies import ActorCriticPolicy
+from stable_baselines3.common.callbacks import CheckpointCallback, EveryNTimesteps
+
+from EvaluationClasses import SaveOnBestTrainingRewardCallback
+from ModificationClasses import ActivationActorCriticPolicy
+
+def SaveGIF(env_id, length, model, algo):
+    # save GIF
+    gif_path = f"record/gif/{algo}_{env_id}.gif"
+
+    images = []
+    obs = model.env.reset()
+    img = model.env.render(mode='rgb_array')
+    for i in range(length):
+        images.append(img)
+        action, _ = model.predict(obs)
+        obs, _, _ ,_ = model.env.step(action)
+        img = model.env.render(mode='rgb_array')
+    imageio.mimsave(gif_path, [np.array(img) for i, img in enumerate(images) if i%2 == 0], fps=29)
+
+def SaveVideo(env_id, video_length, model, algo):
+    video_folder = 'record/videos/'
+    env = DummyVecEnv([lambda: gym.make(env_id)])
+    obs = env.reset()
+    # Record the video starting at the first step
+    env = VecVideoRecorder(env, video_folder, \
+        record_video_trigger=lambda x: x == 0, \
+            video_length=video_length, name_prefix=f"{algo}_{env_id}")
+    env.reset()
+    for _ in range(video_length + 1):
+        action, _ = model.predict(obs)
+        obs, _, _, _ = env.step(action)
+    # Save the video
+    env.close()
+
+def moving_average(values, window):
+    """
+    Smooth values by doing a moving average
+    :param values: (numpy array)
+    :param window: (int)
+    :return: (numpy array)
+    """
+    weights = np.repeat(1.0, window) / window
+    return np.convolve(values, weights, 'valid')
+
+def plot_results(log_folder, title):
+    """
+    plot the results
+
+    :param log_folder: (str) the save location of the results to plot
+    :param title: (str) the title of the task to plot
+    """
+    x, y = ts2xy(load_results(log_folder), 'timesteps')
+    y = moving_average(y, window=50)
+    # Truncate x
+    x = x[len(x) - len(y):]
+
+    fig = plt.figure(title)
+    plt.plot(x, y)
+    plt.xlabel('Number of Timesteps')
+    plt.ylabel('Rewards')
+    plt.title(title)
+    plt.show()
+
+if __name__ == '__main__':
+    # Create log dir
+    log_dir = "tmp/"
+    os.makedirs(log_dir, exist_ok=True)
+
+    # create a gym environment, CartPole-v1 (5e5), HalfCheetah-v2 (2e6)
+    env_id = "CartPole-v1"
+    algo = "PPO"
+    timesteps = 5e5
+    episode = 5
+    # activation function in pytorch:
+    # nn.ReLU(), nn.Tanh(), nn.Sigmoid(), nn.LeakyReLU()
+
+    # create environment
+    env = gym.make(env_id)
+    rewards = []
+    min_len = timesteps
+    opt_str = ['Adam', 'RMSprop', 'SGD']
+    optimizers = [th.optim.Adam, th.optim.RMSprop, th.optim.SGD]
+    for index, optimizer in enumerate(optimizers):
+        for _ in range(episode):
+            # reset the environment
+            env.reset()
+            env = Monitor(env, log_dir)
+            # model definition
+            model = PPO("MlpPolicy", env, verbose=1, policy_kwargs={'optimizer_class': optimizer})
+            # model = PPO(ActivationActorCriticPolicy, env, verbose=1)
+            # model training
+            model.learn(total_timesteps=timesteps)
+            log = load_results(log_dir)
+            rewards.append(log['r'])
+            os.remove(f"{log_dir}monitor.csv")
+            if len(log['r']) < min_len:
+                min_len = len(log['r'])
+        reward_df = pd.DataFrame()
+        for i in range(episode):         
+            reward_df = pd.concat([reward_df, rewards[i][:min_len]], axis=1, ignore_index=True)
+        pkl_file = f"./logs/{algo}_{env_id}_{opt_str[index]}.pkl"
+        reward_df.to_pickle(pkl_file)
+    
+    # # save section
+    # SaveGIF(env_id, 350, model, algo)
+    # SaveVideo(env_id, 350, model, algo)
---
+++ learning_curve_test.py
@@ -0,0 +1,70 @@
+#%%
+import os
+import pandas as pd
+import scipy.stats as stats
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
+
+plt.style.use('seaborn-notebook')
+width = 600  # Get this from LaTeX using \showthe\textwidth
+nice_fonts = {
+              # Use LaTeX to write all text
+              "text.usetex": True,
+              "font.family": "Latin Modern Roman",
+              # Use 10pt font in plots, to match 10pt font in document
+              "axes.labelsize": 11,
+              "font.size": 11,
+              # Make the legend/label fonts a little smaller
+              "legend.fontsize": 8,
+              "xtick.labelsize": 8,
+              "ytick.labelsize": 8,
+              }
+mpl.rcParams.update(nice_fonts)
+# path = '/home/nirnai/Cloud/Uni/TUM/MasterThesis/LaTex/figures'
+
+def set_size(width, fraction=1):
+    """ 
+    Set aesthetic figure dimensions to avoid scaling in latex.
+    Parameters
+    ----------
+    width: float
+        Width in pts
+    fraction: float
+        Fraction of the width which you wish the figure to occupy
+
+    Returns
+    -------
+    fig_dim: tuple Dimensions of figure in inches
+    """
+    # Width of figure
+    fig_width_pt = width * fraction
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+    # Golden ratio to set aesthetic figure height
+    golden_ratio = (5**.5 - 1) / 2
+    # Figure width in inches
+    fig_width_in = fig_width_pt * inches_per_pt
+    # Figure height in inches
+    fig_height_in = fig_width_in * golden_ratio
+    fig_dim = (fig_width_in, fig_height_in)
+    return fig_dim
+
+pkl_dir = '/Users/yuqichengzhu/Desktop/DeepRL-master/20210601/logs/optimizer'
+pkl_paths = [os.path.join(pkl_dir, path) for path in os.listdir(pkl_dir) if path.endswith('.pkl')]
+fig, ax = plt.subplots(1,1,figsize=set_size(width))
+for log in pkl_paths:
+    data = pd.read_pickle(log)
+    means = data.mean(axis=1)
+    se = stats.sem(data, axis=1)
+    low, high = stats.t.interval(0.95, data.shape[1]-1 ,loc=means, scale=se)
+    x = [i for i in range(data.shape[0])]
+    ax.plot(x, means, label=log.split('_')[-1][:-4])
+    ax.fill_between(x, low, high, alpha=0.2)
+plt.legend()
+plt.xlabel('Iteration')
+plt.ylabel('Reward')
+plt.title('PPO performance on CartPole-v1')
+plt.show()
+
+#%%
